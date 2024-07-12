b323f159c2ad8a510af8cc8b773706a9b0218d8b
Maciej Kaczanowski <maciej.emil.kaczanowski@gmail.com>
2024-07-12 15:41:54 +0200

    feat(styles): Add basic css styles

--- src/App.tsx
+++ src/App.tsx
@@ -15,21 +15,21 @@ function App() {
   } = useApi();
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="app-header">
         <p>
           Available  rooms:
         </p>
       </header>
         <br/>
-        <div>
+        <div className="sort">
             <button onClick={handleRoomSortToggle()}>{`Sorted by ${roomsSort === 'DESC' ? "highest ↑": "lowest ↓"} price`}</button>
         </div>
         <br/>
-        <div>
+        <div className="rooms-grid">
           {displayedRooms.map(room => {
             return(
-                <div key={room.id}>
+                <div key={room.id} className="room-card">
                   <div>{room.name}</div>
                   <div>Price: {new Intl.NumberFormat('pl', { style: 'currency', currency: room.price.currencyCode  }).format(
                       room.price.value / 100,
@@ -46,7 +46,7 @@ function App() {
             )
           })}
         </div>
-      <div>
+      <div className="pagination">
         <button disabled={isPrevPageButtonDisable} onClick={handleChangePage('prev')}>Previous page</button>
           {displayedRoomsPage}
         <button disabled={isNextPageButtonDisable} onClick={handleChangePage('next')}>Next page</button>
